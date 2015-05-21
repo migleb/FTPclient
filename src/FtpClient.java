4e9ecda22716d05b5684a1134adbf7ac148fff00
Migle Bucelyte <migle.bucelyte@outlook.com>
2015-05-21 16:48:07 +0300

    Comments added, code optmized

--- src/FtpClient.java
+++ src/FtpClient.java
@@ -34,7 +34,9 @@ public class FtpClient {
     boolean loggedIn = false;
 	
 	/*
+	 * 
 	 * Constructor
+	 *
 	 */
 	public FtpClient () {
 		try {
@@ -56,54 +58,59 @@ public class FtpClient {
 	}
 
 	/*
+	 * 
 	 * Connecting
+	 *
 	 */
 	public Socket connect(String ServerHostName, int port) throws UnknownHostException, IOException {
 		Socket newsocket = new Socket(ServerHostName,port);
 		return newsocket;
 	}
 	
-	
-	
 	/*
+	 * 
 	 * Getting greeting from server
+	 *
 	 */
 	public void getGreeting() throws IOException{
-		input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-
-        do {
-            line=input.readLine();
-            if(responseCode == null)
-                responseCode=line.substring(0,3);
-            System.out.println(line);
-        } while( !(line.startsWith(responseCode) && line.charAt(3) == ' '));
+		getResponse(socket,true);
+//		input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+//
+//        do {
+//            line=input.readLine();
+//            if(responseCode == null)
+//                responseCode=line.substring(0,3);
+//            System.out.println(line);
+//        } while( !(line.startsWith(responseCode) && line.charAt(3) == ' '));
 	}
 	
 	/*
+	 * 
 	 * Function to login
+	 *
 	 */
 	public boolean login() throws IOException {
-		String username = "b15_16171748";
-		String password = "migleb";
-//		String username;
+//		String username = "b15_16171748";
+//		String password = "migleb";
+		String username;
 		String username_cmd;
 		String password_cmd;
 		int command;
 		
 		
-//		System.out.print("Username: ");
-//        username = scan.nextLine();
+		System.out.print("Username: ");
+        username = scan.nextLine();
         username_cmd = "USER " + username + "\n";
         /*
          * Protecting password from echoing
          */
-//        Console console = System.console();
+        Console console = System.console();
 //        String fmt = "";
-//        char[] password = console.readPassword("Password: ");
-//        System.out.println("Password is: " + String.valueOf(password));
-        password_cmd = "PASS " + String.valueOf(password) + "\n";
+//      System.out.println("Password is: " + String.valueOf(password));
         command = sendCommand(socket, username_cmd, true);
         if (command >= 300 && command < 400){
+        	char[] password = console.readPassword("Password: ");
+        	password_cmd = "PASS " + String.valueOf(password) + "\n";
         	command = sendCommand(socket, password_cmd, true);
         	if (command < 200 || command >= 300){
         		System.out.println("Username and/or password is invalid.");
@@ -119,7 +126,9 @@ public class FtpClient {
 	}
 	
 	/*
+	 * 
 	 * Sending command to server and getting response
+	 *
 	 */
 	public int sendCommand (Socket mySocket, String command, boolean needPrinting) throws IOException, NullPointerException {
 		int code;
@@ -131,12 +140,19 @@ public class FtpClient {
 		code = getResponse(mySocket, needPrinting);
         
         if (code == 421) {
-        	reconnectSocket(mySocket);
+        	if (command != "QUIT\n"){
+        		reconnectSocket(mySocket);
+        	} else System.out.println("Goodbye.\nLogout.");
         }
         
         return code;
 	}
 	
+	/*
+	 * 
+	 * Reconnect socket
+	 * 
+	 */
 	public void reconnectSocket(Socket mySocket) throws IOException {
 		socket = connect(ServerHostName, port);
 		System.out.println("Your server has reconnected");
@@ -146,6 +162,12 @@ public class FtpClient {
 		output = new BufferedWriter(new OutputStreamWriter(mySocket.getOutputStream()));
 	}
 	
+	
+	/*
+	 * 
+	 * Get response
+	 * 
+	 */
 	public int getResponse(Socket mySocket, boolean needPrinting) throws IOException{
 		String line = null;
 	    String responseCode = null;
@@ -167,37 +189,50 @@ public class FtpClient {
 	}
 
 	/*
+	 * 
 	 * Transform code to integer
+	 *
 	 */
 	public int getResponseCode(String responseCode){
 		int code = Integer.valueOf(responseCode);
 		return code;
 	}
 	
+	/*
+	 * 
+	 * Change directory
+	 * 
+	 */
 	public void changeDirectory() throws IOException {
-		String dirname_cmd;
+		String dirnameCmd;
 		String dirname;
 		int temp;
 		
 		System.out.print("Go to: ");
 		dirname = scan.nextLine();
-		dirname_cmd = "CWD " + dirname + "\n";
-		temp = sendCommand(socket, dirname_cmd, true);
+		dirnameCmd = "CWD " + dirname + "\n";
+		temp = sendCommand(socket, dirnameCmd, true);
 		if (temp != 250) {
-			System.out.println("Error occurred accessing directory " + dirname);
+			System.out.println("Error occurred accessing directory /" + dirname);
 		} else {
-			System.out.println("Current working directory is " + dirname);
+			System.out.println("Current working directory is /" + dirname);
 		}
 	}
 	
+	/*
+	 * 
+	 * Download file
+	 * 
+	 */
 	public int getFile() throws IOException {
 		Socket data = null;
 		String fileName;
 		String command;
 		int code;
-		byte[] buffer = new byte[0];
 
-		data = enterPassiveMode(data);
+		if ((data = enterPassiveMode(data)) == null){
+			return -1;
+		}
 		
 //		BufferedInputStream input2 = new BufferedInputStream(data.getInputStream());
 //		DataInputStream input2 = new DataInputStream(data.getInputStream());
@@ -215,118 +250,40 @@ public class FtpClient {
 			return -1;
 		}
 		
-		System.out.println("Downloading...");
-		
-//		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-//		DataOutputStream dos = new DataOutputStream(baos);
-		
-//	    BufferedOutputStream bos = new BufferedOutputStream(outStream);
-		
-//		int got = 1;
-//		while (got > 0) {
-//			System.out.println("lala");
-//			String temp = "";
-//			line = null;
-//		    responseCode = null;
-//		    
+		System.out.println("Downloading...");  
 		    
 		
 		byte[] aByte = new byte[10000];
 		int bytesRead;
-		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 		
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 		DataInputStream in = new DataInputStream(data.getInputStream());
 		
-        if (data.getInputStream() != null) {
-
+        if (in != null) {
             FileOutputStream fos = null;
             BufferedOutputStream bos = null;
             try {
-                fos = new FileOutputStream( fileName );
+                fos = new FileOutputStream(fileName);
                 bos = new BufferedOutputStream(fos);
                 
-                int bb = 0;
                 do {
-                        bytesRead = data.getInputStream().read(aByte);
-                        	System.out.println("b=" + bytesRead);
-                        	if (bytesRead > 0) {
-                        	bb += bytesRead;
-                        	baos.write(aByte, 0, bytesRead);
-//                        	bos.write(aByte);
-                        	}
-                        	
-                            
+                	bytesRead = in.read(aByte);
+                    if (bytesRead > 0) {
+                		baos.write(aByte, 0, bytesRead);
+//                      bos.write(aByte);
+                	}   
                 } while (bytesRead != -1);
 
-                System.out.println("saddas"+baos.toByteArray().length);
                 bos.write(baos.toByteArray());
                 baos.close();
                 bos.flush();
                 bos.close();
-                System.out.println("saddas"+bb);
                 fos.close();
             } catch (IOException ex) {
-                // Do exception handling
+                
             }
         }
-		
-		
-		
-		
-		
-		
-		
-//		    do {
-		    	
-		    	
-//		    	line = input2.read
-//				if (line != null) {
-//					if(responseCode == null)
-//						responseCode = line.substring(0, 3);
-//					System.out.println(line);
-//					temp = line;
-//				} else break;
-//		    } while ( !(line.startsWith(responseCode) && line.charAt(3) == ' '));
-		    
-//			do {
-//				System.out.println("lalala");
-//				try {
-////					line = String.valueOf(input2.read());
-//					if (input2.available() > 0) {
-//						line = String.valueOf(input2.read());
-//						if((responseCode / 1000) != 0) {
-//							responseCode = responseCode * 10 + Integer.valueOf(line);
-//						}
-//						
-//						temp = line;
-//						
-//	//					System.arraycopy(buffer, 0, c, 0, buffer.length);
-//	//					System.arraycopy(temp.getBytes(), 0, c, buffer.length, temp.getBytes().length);
-//	//					buffer = c;
-//	//					baos(temp);
-//						
-//						dos.writeInt(Integer.valueOf(temp));
-//						dos.flush();
-//						byte[] c = baos.toByteArray();
-//						outStream.write(c);
-//						System.out.println(line);
-//						System.out.println("Im here");
-//					} else break;
-//				} catch (EOFException e){
-//					System.out.println("End of file");
-//					break;
-//				}
-//				System.out.println("Here now");
-//				
-//			} while ( !(line.startsWith("150") && line.charAt(3) == ' '));
-//			got = temp.length();
-//		}
-//		baos.writeTo(outStream);
-//		System.out.println("finally");
-//		outStream.close();
-//		baos.close();
-//		dos.close();
-//		
+        
 		code = getResponse(socket,true);
 		
 		if (code < 200 || code >= 300) {
@@ -337,6 +294,14 @@ public class FtpClient {
 		return 1;
 		
 	}
+	
+	
+	/*
+	 * 
+	 * 	Get list
+	 * 
+	 * 
+	 */
 	
 	public int getList() throws IOException{
 		String command;
@@ -393,6 +358,11 @@ public class FtpClient {
 		return 1;
 	}
 	
+	/*
+	 * 
+	 * Enter passive mode
+	 * 
+	 */
 	public Socket enterPassiveMode(Socket data) throws IOException {
 		String temp = "";
 		String command = "PASV\n";
@@ -417,7 +387,7 @@ public class FtpClient {
             if (line != null) {
             	if(responseCode == null)
 	                responseCode=line.substring(0,3);
-            System.out.println(line);
+            //System.out.println(line);
             temp = temp + line;
             } else break;
         } while( !(line.startsWith(responseCode) && line.charAt(3) == ' '));
@@ -459,13 +429,23 @@ public class FtpClient {
 	}
 	
 	/*
+	 * 
 	 * Function to print menu
+	 *
 	 */
 	public void showMenu() {
 		System.out.println("\t---MENU---");
-		System.out.println("\t1 : Download file\n\t2 : Change working directory\n\t3 : Show list of files and folders\n\t0 : Quit (Logout)");
+		System.out.println("\t1 : Download file\n"
+						 + "\t2 : Change working directory\n"
+						 + "\t3 : Show list of files and folders\n"
+						 + "\t0 : Quit (Logout)");
 	}
 	
+	/*
+	 * 
+	 * Action chooser
+	 * 
+	 */
 	public int getAction() throws IOException, NullPointerException {
 		int action;
 		while (true) {
@@ -492,19 +472,7 @@ public class FtpClient {
 					}
 					
 					case 0: {
-						sendCommand(socket,"QUIT\n",true);
-//						String line = null;
-//					    String responseCode = null;
-//						output.write("QUIT\n");
-//						output.flush();
-//						
-//						do {
-//				            line=input.readLine();
-//				            if(responseCode == null)
-//				                responseCode=line.substring(0,3);
-//				            System.out.println(line);
-//				        } while( !(line.startsWith(responseCode) && line.charAt(3) == ' '));
-						
+						sendCommand(socket,"QUIT\n",true);				
 						return 1;
 					}
 				
@@ -519,7 +487,9 @@ public class FtpClient {
 	}
 	
 	/*
+	 * 
 	 * Main function
+	 *
 	 */
 	public static void main (String[] args) {
 		FtpClient client = new FtpClient();
